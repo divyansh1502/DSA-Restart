336d7574e63a341c44ea96b83f40cb6430ac4e2c
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-05 16:15:06 +0530

    Rock Paper Scissor

--- Practice_Ques_CS/atm.java
+++ Practice_Ques_CS/atm.java
@@ -1,9 +1,27 @@
-
 import java.util.Scanner;
 
 public class atm {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        syso
+        System.out.println("Enter 1 for Balance Enquiry:\nEnter 2 for Deposit:\nEnter 3 for Withdraw:\nEnter 4 for Exit:\n");
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.println("Balance Check");
+                break;
+            case 2:
+                System.out.println("Deposit");
+                break;
+            case 3:
+                System.out.println("Withdraw");
+                break;
+            case 4:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
     }
 }
---
+++ Practice_Ques_CS/rockPaperScissor.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class rockPaperScissor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("R for Rock\nP for Paper\nS for Scissor");
+        System.out.print("Enter a input: ");
+        char user = sc.next().trim().toLowerCase().charAt(0);
+
+        int computer = (int)(Math.random()*3);
+        //0 --> Rock
+        //1 --> Paper
+        //2 --> Scissor
+        if(computer == 0 && user == 'r') {
+            System.out.println("Computer Choosed: Rock");
+            System.out.println("Draw");
+        } else if(computer == 0 && user == 'p') {
+            System.out.println("Computer Choosed: Rock");
+            System.out.println("Computer Wins");
+        } else if(computer == 0 && user == 's') {
+            System.out.println("Computer Choosed: Rock");
+            System.out.println("User wins");
+        } else if(computer == 1 && user == 'p') {
+            System.out.println("Computer Choosed: Paper");
+            System.out.println("Draw");
+        } else if(computer == 1 && user == 's') {
+            System.out.println("Computer Choosed: Paper");
+            System.out.println("User wins");
+        } else if(computer == 1 && user == 'r') {
+            System.out.println("Computer Choosed: Paper");
+            System.out.println("Computer wins");
+        } else if(computer == 2 && user == 's') {
+            System.out.println("Computer Choosed: Scissor");
+            System.out.println("Draw");
+        } else if(computer == 2 && user == 'r') {
+            System.out.println("Computer Choosed: Scissor");
+            System.out.println("User wins");
+        } else if(computer == 2 && user == 'p') {
+            System.out.println("Computer Choosed: Scissor");
+            System.out.println("Computer wins");
+        } else {
+            System.out.println("Invalid input by user");
+        }
+    }
+}
