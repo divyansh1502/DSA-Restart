46bdfdd3b32075669af380ae88d400086688b1b2
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-13 23:22:07 +0530

    ArrayList

---
+++ Array/arrayList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arrayList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Syntax
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // list.add(67);
+        // list.add(669);
+        // list.add(01);
+        // list.add(13);
+        // list.add(24);
+        // list.add(78);
+         //input
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+
+        System.out.println(list.contains(669));
+
+        list.set(0, 99);
+
+        list.remove(2);
+
+       
+
+        System.out.println(list);
+    }
+}
