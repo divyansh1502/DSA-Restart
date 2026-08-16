62dbb3e2642e9a7cc276cfb091ed2a8e1181603e
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-16 23:02:03 +0530

    LC27

---
+++ Linear-Search/LC27.java
@@ -0,0 +1,18 @@
+
+public class LC27 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,2,3,0,4,2};
+        int target = 2;
+        System.out.println(removeElement(arr, target));
+    }
+    static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
