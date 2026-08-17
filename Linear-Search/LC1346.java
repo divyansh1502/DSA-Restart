81ba90366d618c5f8ada1218ba081797c708259b
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-17 17:20:40 +0530

    LC1346

---
+++ Linear-Search/LC1346.java
@@ -0,0 +1,17 @@
+public class LC1346 {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 5, 3};
+        System.out.println(checkIfExist(arr));
+    }
+    static boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(i != j && arr[i] == 2 * arr[j]) {
+                     return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
