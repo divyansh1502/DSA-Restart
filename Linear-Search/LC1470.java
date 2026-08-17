a81bda1616e8d5be4bdbc54c709c78126cebbed1
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-17 18:10:41 +0530

    LC1470

---
+++ Linear-Search/LC1470.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class LC1470 {
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,3,4,7};
+        int n = 3;
+
+        System.out.println(Arrays.toString(shuffle(arr, n)));
+    }
+    static int[] shuffle(int[] nums, int n) {
+        int[] newArr = new int[nums.length];
+        
+        for (int i = 0; i < n; i++) {
+            newArr[2 * i] = nums[i];
+            newArr[2 * i + 1] = nums[i + n];
+        }
+        return newArr;
+    }
+}
