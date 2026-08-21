3be13d66a6e9a82196026f1832e3fe5ac18e086a
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-21 14:52:57 +0530

    Yreahhhh\

---
+++ Binary-Search/BinarySearchQues/LC1095.java
@@ -0,0 +1,24 @@
+public class LC1095 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,3,1};
+        int target = 3;
+        System.out.println(findPeakIndex(arr, target));
+    }
+    static int findPeakIndex(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] < arr[mid + 1]) {
+                if(target < arr[mid]) {
+                    end = mid + 1;
+                } else if(target > arr[mid]) {
+                    start = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+        }
+        return -1;
+    }
+}
