443e639fec5400ea9e9e78bb56c8571bf4f06b57
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-27 18:37:19 +0530

    Count in Rotated Array

---
+++ Binary-Search/BinarySearchQues/RotationCount.java
@@ -0,0 +1,29 @@
+public class RotationCount {
+    public static void main(String[] args) {  
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(count(arr));
+    }
+
+    static int count(int[] arr) {
+        int pivot = pivot(arr);
+        return pivot + 1;
+    }
+    static int pivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } if(mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            } if(arr[start] > arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
