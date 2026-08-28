6bbd0d179e749dd6c19503f608a2f61559708d2e
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-28 22:30:11 +0530

    CoderArmy

---
+++ Binary-Search/BinarySearchQues/test.java
@@ -0,0 +1,14 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        long b;
+        b = a;
+        System.out.println(b);
+
+        String c = "a";
+        String d = "b";
+        String e = c + d;
+
+        System.out.println(e);
+    }
+}
