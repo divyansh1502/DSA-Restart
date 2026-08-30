820a1490b73cbd08f6be1a10f4398421bed3bacf
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-08-30 15:56:52 +0530

    OOPS

--- Sorting/oops/classes.java
+++ Sorting/oops/classes.java
@@ -1,14 +1,14 @@
 public class classes {
     public static void main(String[] args) {
 
-        Students s1 = new Students();
+        Student s1 = new Student();
 
         s1.name = "Divyansh";
         s1.age = 22;
         s1.college = "AIET";
         s1.fees = 60000;
 
-        Students s2 = new Students();
+        Student s2 = new Student();
         
         s2.name = "Aryan";
         s2.age = 23;
@@ -16,11 +16,23 @@ public class classes {
         s2.fees = 60000;
 
         System.out.println(s1.name);
+        s1.markAttendence();
+        s1.display();
+
+        s2.markAttendence();
+        s2.display();
     }
 }
-class Students {
+class Student {
     String name;
     int age;
     String college;
     int fees;
+
+    void markAttendence() {
+        System.out.println("Attendence marked by : " + name);
+    }
+    void display() {
+        System.out.println("Name: " + name + ", age: " + age + ", college: " + college + ", fees: " + fees);
+    }
 }
