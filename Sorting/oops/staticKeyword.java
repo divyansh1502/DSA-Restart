308a9efd506cde28f719ba09ccf038fab58fcda2
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-09-01 18:52:12 +0530

    Static keyword

---
+++ Sorting/oops/staticKeyword.java
@@ -0,0 +1,30 @@
+public class staticKeyword {
+    public static void main(String[] args) {
+        Student s1 = new Student("Divyansh", 22, 101);
+        Student s2 = new Student("Shagun", 21, 102);
+        
+        // Student.college = "AIET";
+
+        s1.display();   
+        s2.display();   
+
+    }
+}
+class Student {
+    String name;
+    int age;
+    int rollNumber;
+    static String college;
+
+    Student(String name, int age, int rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Roll Number: " + rollNumber + ", College: " + college);
+    }
+    static {
+        college = "AIET";
+    }
+}
